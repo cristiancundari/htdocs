76e64a54c14d2ebbdc1c4797cb289c5ad105c7d5
Cristian <cristian3095@hotmail.it>
2018-03-10 03:43:12 +0100

    Changed DB hosting

--- Config.php
+++ Config.php
@@ -8,18 +8,32 @@
      * define("DB_PASSWORD", "W25Af4o8OPV4bZwH");
      * define("DB_DATABASE", "birdbase");
      * 
+     * ---------------------------------------------------
+     * 
      * LINK: https://www.freemysqlhosting.net/login/
      * email: ficitaroj@emailsy.info
      * passw: pippofranco123
      * 
-     * ------------------------------------------------
      * ACCEDERE A PHPMYADMIN
      * LINK http://www.phpmyadmin.co/
      * CREDENZIALI (vedi sotto)
+     * 
+     * define("DB_HOST", "sql11.freemysqlhosting.net");
+     * define("DB_USER", "sql11225565");
+     * define("DB_PASSWORD", "k1iCPhL1jU");
+     * define("DB_DATABASE", "sql11225565");
+     * 
+     * ---------------------------------------------------
+     * 
+     * LINK: https://www.heliohost.org/
+     * email: cristian3095@hotmail.it
+     * passw: pippo123
+     * 
+     * ---------------------------------------------------
      */
-    define("DB_HOST", "sql11.freemysqlhosting.net");
-    define("DB_USER", "sql11225565");
-    define("DB_PASSWORD", "k1iCPhL1jU");
-    define("DB_DATABASE", "sql11225565");
+    define("DB_HOST", "ccundari.heliohost.org");
+    define("DB_USER", "ccundari_android");
+    define("DB_PASSWORD", "yl9sX9ZXseJb");
+    define("DB_DATABASE", "ccundari_birdbase");
 
 ?>
